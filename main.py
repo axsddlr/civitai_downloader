97b0560d2ef2adf889fc3f0563b8c9bd4da9cc54
Andre Saddler <dresadd09691@gmail.com>
2023-02-19 22:11:17 -0500

    init: code base rewrite
    Signed-off-by: Andre Saddler <dresadd09691@gmail.com>

--- main.py
+++ main.py
@@ -1,307 +1,65 @@
-# Importing the argparse module
-import argparse
+import asyncio
+import httpx
 import json
-import os
-import re
-import threading
-
-import requests
-from tqdm import tqdm
-
-from src.downloader import downloadFile
-from src.utils.utils import headers
-from src.utils.utils import readMemory, writeMemory, printv
-
-# Creating an argument parser object
-parser = argparse.ArgumentParser()
-
-# Adding the arguments for the file names
-parser.add_argument("--lora", action="store_true", help="Load LORA.txt instead of id.txt")
-parser.add_argument("--ti", action="store_true", help="Load TextualInversion.txt instead of id.txt")
-parser.add_argument("--ckpt", action="store_true", help="Load Checkpoint.txt instead of id.txt")
-parser.add_argument("--hn", action="store_true", help="Load Hypernetwork.txt instead of id.txt")
-
-# Parsing the arguments
-args = parser.parse_args()
 
 
-def get_model_id():
-    """
-    It checks if the file `id.txt` exists, if it does not exist, it creates it, if it exists, it reads the file and returns
-    a list of all the numbers in the file.
-    :return: A list of all the model ids.
-    """
-    # Checking if the user specified a different file name
-    if args.lora:
-        file_name = "LORA.txt"
-    elif args.ti:
-        file_name = "TextualInversion.txt"
-    elif args.ckpt:
-        file_name = "Checkpoint.txt"
-    elif args.hn:
-        file_name = "Hypernetwork.txt"
-    else:
-        file_name = "id.txt"
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
-    # Checking if the file exists. If it does not exist, it will create it.
-    if not os.path.exists(file_name):
-        with open(file_name, "w") as f:
-            f.write("")
-            print(f"{file_name} file created, run the script again")
-    else:
-        with open(file_name, "r") as f:
-            # Getting all the numbers from the file and putting them in a list.
-            model_id_list = [re.search("\d+", line.strip()).group() for line in f if line.strip()]
-        return model_id_list
+api_key = config["civitai_api_key"]
+APIKEY = f"Bearer {api_key}"
 
 
-# Define the list only once
-aListOfAllDownloadedModels = []
+async def get_all_models():
+    async with httpx.AsyncClient() as client:
+        all_models = []
+        params = {
+            "types": ("TextualInversion", "Hypernetwork", "LORA", "AestheticGradient"),
+            "period": "AllTime",
+            "limit": 100,
+            "page": 1
+        }
+        url = "https://civitai.com/api/v1/models"
+        querystring = {"sort": "Newest", "favorites": "true"}
+        headers = {"Authorization": APIKEY}
 
-# Call the readMemory function to get updated values
-updatedList = readMemory()
+        response = await client.get(url, headers=headers, params=querystring)
+        response_json = response.json()
+        all_models.extend(response_json["items"])
 
-# Update the list with the updated values
-aListOfAllDownloadedModels = updatedList
+        for page_num in range(2, response_json["metadata"]["totalPages"] + 1):
+            params["page"] = page_num
+            response = await client.get(url, headers=headers, params=querystring)
+            response_json = response.json()
+            all_models.extend(response_json["items"])
 
+        params = {
+            "types": "Checkpoint",  # All types: TextualInversion, Hypernetwork, Checkpoint, LORA, AestheticGradient
+            "period": "AllTime",
+            "limit": 30,
+            "page": 1
+        }
 
-def getModels(session):
-    """
-    It takes a session object and returns a list of model objects
+        response = await client.get(url, headers=headers, params=querystring)
+        response_json = response.json()
+        all_models.extend(response_json["items"])
 
-    :param session: the session object that you created earlier
-    :return: A list of dictionaries.
-    """
-    model_ids = get_model_id()
-    models = []
-    for model_id in model_ids:
-        model_response = session.get(f"https://civitai.com/api/v1/models/{str(model_id)}", headers=headers)
-        try:
-            model_data = json.loads(model_response.text)
-        except ValueError:
-            print(f"Invalid JSON response for model id: {model_id}, URL: {model_response.url}")
-            continue
-        models.append(model_data)
-    if len(models) == 0:
-        print("id.txt is empty, please add model ids to it")
-        exit()
-    else:
-        return models
-
-
-session_downloadedFileCount = 0
-session_downloadedBytes = 0
-failedHashes = 0
-
-
-def isModelAlreadyInMemory(modelIdStr, model):
-    """
-    If the model used as a key, in the dictionary of all models, returns the same version id as the model's version id, then
-    the model has already been downloaded
-
-    :param modelIdStr: The model's id, as a string
-    :param model: The model object from the API
-    :return: A boolean indicating whether the model has already been downloaded
-    """
-    if modelIdStr in aListOfAllDownloadedModels:
-        if aListOfAllDownloadedModels[modelIdStr] == model["modelVersions"][0]["id"]:
-            print("Skipping model, already in memory", model["name"])
-            return True
-        print("Update found for model", model["name"])
-    return False
+        print(f"Found {len(all_models)} models")
+        return all_models
 
 
-def findFiles(model):
-    """
-    It takes a model as input and returns a list of all the files in the model's directory.
-
-    :param model: The model you want to find files for
-    """
-    files = []
-    acceptableTypes = ["Pruned Model", "Model", "VAE"]
-    modelFound = False
-    for file in model["modelVersions"][0]["files"]:
-        # print(f"{file}")
-        if file["type"] in acceptableTypes:
-            # if file["type"] == "Model" and modelFound:  # Prevent multiple models from being downloaded
-            #    continue
-            # if file["type"] == "Model":
-            #    modelFound = True
-            print(f"Added {file['name']}")
-            files.append(file)
-
-    return files
+async def get_model_versions_and_ids():
+    all_models = await get_all_models()
+    # Here, the modified model_versions list comprehension iterates over each model in all_models, then over each
+    # version in the modelVersions list for that model, and returns the "id" attribute for each version.
+    model_versions = [version["id"] for model in all_models for version in model["modelVersions"]]
+    # This is a list comprehension that iterates over each model in all_models, then over each version in the
+    # modelVersions list for that model, and returns the "id" attribute for each version.
+    model_ids = [model["modelVersions"][0]["modelId"] for model in all_models]
+    return model_versions, model_ids
 
 
-# It creates a class called File.
-class File:
-    def __init__(self, name, downloadUrl, imageUrl, hash):
-        self.name = name
-        self.downloadUrl = downloadUrl
-        self.imageUrl = imageUrl
-        self.hash = hash
-
-
-def getFiles(model):
-    """
-    It takes a model object and returns a list of File objects
-
-    :param model: The model object that we're getting the files for
-    :return: A list of File objects.
-    """
-    files = findFiles(model)
-    filesAsObjects = []
-
-    safetensorfound = False
-    pickletensorfound = False
-    for file in files:
-        # print(f"{file}")
-        # find a safetensor first, if any
-        if file["format"] == "SafeTensor":
-            # use this one
-            safetensorfound = True
-            modelCkptFileName = file["name"]
-            modelCkptDownloadLink = file["downloadUrl"]
-            try:
-                modelCkptHash = file["hashes"]["SHA256"]
-            except KeyError:
-                modelCkptHash = None
-            modelImage = model["modelVersions"][0]["images"][0]["url"]
-            break
-    if safetensorfound == False:
-        # grab a Pickle instead
-        pickletensorfound = False
-        for file in files:
-            if file["format"] == "PickleTensor":
-                # use this one
-                pickletensorfound = True
-                modelCkptFileName = file["name"]
-                modelCkptDownloadLink = file["downloadUrl"]
-                try:
-                    modelCkptHash = file["hashes"]["SHA256"]
-                except KeyError:
-                    modelCkptHash = None
-                modelImage = model["modelVersions"][0]["images"][0]["url"]
-                break
-    if pickletensorfound == False and safetensorfound == False:
-        # Houston we have a problem
-        print(f"Error, neither a safetensor nor pickletensor found... update your code")
-    else:
-        filesAsObjects.append(File(modelCkptFileName, modelCkptDownloadLink, modelImage, modelCkptHash))
-
-    return filesAsObjects
-
-
-def iterateAModel(model):
-    """
-    > Iterate through all the layers in a model and print out the layer name and the number of parameters in that layer
-
-    :param model: the model to iterate
-    """
-    global aListOfAllDownloadedModels
-
-    files = getFiles(model)
-
-    # It creates a directory called JSON if it doesn't already exist.
-
-    # Checking if there are any files in the model.
-    if len(files) > 0:
-        os.makedirs("JSON", exist_ok=True)
-        with open(os.path.join("JSON", files[0].name + ".json"), "w") as f:
-            json.dump(model, f, indent=4)
-    else:
-        print("No files found, skipping...")
-        return
-
-    # Iterating through all the files in the model.
-    for file in files:
-        downloadSuccessful = downloadFile(file.downloadUrl, file.imageUrl, modelType=model["type"], hash=file.hash)
-
-        if not downloadSuccessful:
-            printv(f"Download failed, {file.name} skipping...")
-            return
-
-        # Iterating through all the model versions and getting all the trained words.
-        trainedWords = [item for sublist in model["modelVersions"] for item in sublist["trainedWords"]]
-
-        if len(trainedWords) > 0:
-            trainedWords = list(set(trainedWords))
-            model_to_file = {
-                "LORA": "LORA/" + file.name + ".txt",
-                "TextualInversion": "TextualInversion/" + file.name + ".txt",
-                "Checkpoint": "Checkpoint/" + file.name + ".txt",
-            }
-
-            file_path = model_to_file.get(model["type"], None)
-            if not file_path:
-                print("No file path provided, skipping...")
-            elif os.path.exists(file_path):
-                print(f"File path exists {file_path}, skipping...")
-            else:
-                with open(file_path, "w+", encoding="utf-8") as f:
-                    f.write(', '.join(trainedWords))
-
-    # Adding the model to the list of all downloaded models.
-    aListOfAllDownloadedModels[model["id"]] = model["modelVersions"][0]["id"]
-
-
-def waitUntilAllThreadsAreDone(allThreads):
-    """
-    It waits until all the threads in the list `allThreads` are done
-
-    :param allThreads: a list of all the threads that are running
-    """
-    for thread in allThreads:
-        thread.join()
-
-
-allThreads = []
-multiThreadLimit = 20
-session = requests.Session()
-allModels = getModels(session)
-
-# Iterating through all the models in the list `allModels` and printing out a progress bar.
-# Downloading all the models from the session.
-for model in tqdm(getModels(session), desc="Downloading models", leave=True, position=0):
-    try:
-
-        modelExists = isModelAlreadyInMemory(model["id"], model)
-    except KeyError:
-        print(f"KeyError, model: {model}")
-        continue
-
-    modelIsCheckpoint = model["type"] == "Checkpoint"
-
-    if not modelExists:
-        if modelIsCheckpoint:  # If the model is a checkpoint then go in single threading to avoid errors
-            iterateAModel(model)
-            continue
-        else:
-            #  Start new thread
-            newThread = threading.Thread(target=iterateAModel, args=(model,))
-            newThread.start()
-            allThreads.append(newThread)
-
-    if session_downloadedFileCount > 10 or session_downloadedBytes > 1024 * 1024 * 1024:  # Downloaded 50 files or 1GB
-        session_downloadedBytes = 0
-        writeMemory(aListOfAllDownloadedModels)
-
-    if len(threading.enumerate()) > 20:  # 50 threads or a checkpoint
-        print("Waiting for threads to finish...")
-        waitUntilAllThreadsAreDone(allThreads)  # Wait up for all the downloads to finish
-
-    if len(threading.enumerate()) > 20 or model["type"] == "Checkpoint":  # 50 threads or a checkpoint
-        waitUntilAllThreadsAreDone(allThreads)  # Wait up for all the downloads to finish
-    else:
-        # Handle the case where the 'id' key is not in the model dictionary
-        continue
-        # print("Error: the 'id' key is not in the model dictionary.")
-
-    writeMemory(aListOfAllDownloadedModels)
-
-# It waits until all the threads in the list `allThreads` are done.
-waitUntilAllThreadsAreDone(allThreads)
-# Writing the list of all downloaded models to a file called `memory.json`.
-writeMemory(aListOfAllDownloadedModels)
-# Printing the number of failed hashes.
-print(f"Failed hashes: {failedHashes}")
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    model_versions, model_ids = loop.run_until_complete(get_model_versions_and_ids())
+    print(model_versions)
